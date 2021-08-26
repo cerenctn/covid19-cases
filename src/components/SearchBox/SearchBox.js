d0fcd02a3289664b32d46e12188162dfe4d0b44f
Ceren <cerenctn@hotmail.co.uk>
2021-08-26 21:56:58 +0100

    initial commit

---
+++ src/components/SearchBox/SearchBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './searchbox.css';
+
+const SearchBox = ({placeholder, handleChange}) =>{
+    return(
+        <input type='search' className='search' 
+        placeholder={placeholder} 
+        onChange={handleChange}></input>
+    )
+}
+
+export default SearchBox;
